fd5cd9ee614304f3076763e80d5b75a546cb35dd
Pravinkadam2704 <pravinkadam2704@gmail.com>
2025-07-12 22:14:35 +0530

    Commit project

---
+++ src/main/java/com/delieveryBoy/DelieveryBoyApplication.java
@@ -0,0 +1,13 @@
+package com.delieveryBoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelieveryBoyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelieveryBoyApplication.class, args);
+	}
+
+}
