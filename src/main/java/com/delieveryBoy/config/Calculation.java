e5c4ee2bd33d1e56108d59a250fb3f555fa80df1
Pravinkadam2704 <pravinkadam2704@gmail.com>
2025-07-12 22:23:10 +0530

    Commit project 2

---
+++ src/main/java/com/delieveryBoy/config/Calculation.java
@@ -0,0 +1,11 @@
+package com.delieveryBoy.config;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculate")
+public class Calculation {
+
+}
